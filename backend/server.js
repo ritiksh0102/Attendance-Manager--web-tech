3d77a7f15ef1335479c6532aab3bf3ce0f2688f7
Ritik <56392858+ritiksh0102@users.noreply.github.com>
2020-12-07 11:28:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,15 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-const app =express(); 
+const app =express(); //this will start our expess sever 
 const port = process.env.PORT || 4000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json());//allow us to parse json
 
-const uri = process.env.ALTAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true,useCreateIndex: true, useUnifiedTopology: true });
-const connection = mongoose.connection;
+const uri = process.env.ALTAS_URI;//connecting environment variable
+mongoose.connect(uri, { useNewUrlParser: true,useCreateIndex: true, useUnifiedTopology: true });//connection made
+const connection = mongoose.connection;//connection complete
 
 connection.once('open', () => {
     console.log("MongoDB database Connection established successfully")
