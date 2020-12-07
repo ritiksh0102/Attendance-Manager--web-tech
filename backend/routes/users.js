3e1d704a3b5c97c27ad9d31889ef66cbbfe53011
Ritik <56392858+ritiksh0102@users.noreply.github.com>
2020-12-07 11:39:40 +0530

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,16 +1,16 @@
-const router = require('express').Router();
-let user = require('../models/user.model');
+const router = require('express').Router();//routes we r creating
+let user = require('../models/user.model');//loading model
 
-router.route('/').get((req, res) => {
+router.route('/').get((req, res) => {         //first in point that handles http request
   user.find()
-    .then(users => res.json(users))
+    .then(users => res.json(users))//return in json format
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/add').post((req, res) => {
+router.route('/add').post((req, res) => {   //to add new user
   const username = req.body.username;
 
-  const newUser = new user({username});
+  const newUser = new user({username});   //new instance of user
 
   newUser.save()
     .then(() => res.json('User added!'))
